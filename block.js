1d57d7476bda77106e043890e691a78911d33b74
SreenityaPonipireddy <75820726+SreenityaPonipireddy@users.noreply.github.com>
2021-01-29 09:24:33 -0600

    Add files via upload
    AYAYAYAY It worked :)

---
+++ block.js
@@ -0,0 +1,35 @@
+class Block{
+    //Declaring all of the properties of box
+       constructor(x,y, width, height){
+              
+           var options={
+               restitution:0.8,
+               density:1.0,
+               friction:1.0
+           }
+   
+           //"this" represents something to replace and is replaced with the name
+           this.body=Bodies.rectangle(x,y,width,height,options);
+           this.height=height;
+           this.width=width;
+           World.add(world,this.body);
+
+       }
+
+      
+       display(){
+        
+           var pos=this.body.position;
+           var angle=this.body.angle;
+            push();
+            translate(pos.x,pos.y)
+            rotate(angle)
+            rectMode(CENTER);
+            strokeWeight(4);
+            stroke("LightBlue");
+            fill("LightSeaGreen");
+            rect(0,0,this.width,this.height);
+            pop();
+   
+       }
+    }
